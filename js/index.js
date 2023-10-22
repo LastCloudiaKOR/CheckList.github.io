faf77ffbd501c370cd734b85a3114bfd53718b3a
LastCloudiaKOR <korlastcloudia@gmail.com>
2023-10-22 15:09:13 +0900

    dd
    dd

--- js/index.js
+++ js/index.js
@@ -6,7 +6,6 @@ const seasonalUnitCnt = 3;
 const collabUnitCnt = 6;
 const monsterCnt = 10;;
 
-
 const rlArkCnt = 0;
 const rslArkCnt = 0;
 const reArkCnt = 0;
@@ -21,7 +20,7 @@ const srslArkCnt = 1;
 const sreArkCnt = 0;
 const srArkTcnt = srcArkCnt + srlArkCnt + srgArkCnt + srslArkCnt + sreArkCnt;
 
-const ssrgArkCnt = 43;
+const ssrgArkCnt = 44;
 const ssrcArkCnt = 4;
 const ssreArkCnt = 1;
 const ssrslArkCnt = 1;
@@ -52,22 +51,26 @@ const unitInfo = {
     monster :   { rate : "monster",  cnt : monsterCnt,      title : "몬스터(" + monsterCnt + ")"         }, 
 };
 const arkInfo = {
-    ur :  { rate : "ur",  tcnt : urArkTCnt,  gcnt : urgArkCnt,  eCnt : ureArkCnt, title : "UR(" + urArkTCnt +")"}, 
+    ur :  { rate : "ur",  tcnt : urArkTCnt,  gcnt : urgArkCnt,  eCnt : ureArkCnt,  title : "UR(" + urArkTCnt +")"}, 
     lr :  { rate : "lr",  tcnt : lrArkTCnt,  gcnt : lrgArkCnt,  eCnt : lreArkCnt,  slimitCnt : lrslArkCnt,  limitCnt : lrlArkCnt,  collabCnt : lrcArkCnt,  title : "LR(" + lrArkTCnt +")"}, 
     ssr : { rate : "ssr", tcnt : ssrArkTCnt, gcnt : ssrgArkCnt, eCnt : ssreArkCnt, slimitCnt : ssrslArkCnt, limitCnt : ssrlArkCnt, collabCnt : ssrcArkCnt, title : "SSR(" + ssrArkTCnt +")" }, 
-    sr :  { rate : "sr",  tcnt : srgArkCnt,  gcnt : srgArkCnt,  eCnt : sreArkCnt,  slimitCnt : srslArkCnt,   limitCnt : srlArkCnt,  collabCnt : srcArkCnt,  title : "SR(" + srArkTcnt +")"   }, 
+    sr :  { rate : "sr",  tcnt : srgArkCnt,  gcnt : srgArkCnt,  eCnt : sreArkCnt,  slimitCnt : srslArkCnt,  limitCnt : srlArkCnt,  collabCnt : srcArkCnt,  title : "SR(" + srArkTcnt +")"   }, 
     r :   { rate : "r",   tcnt : rArkTcnt,   gcnt : rgArkCnt,   eCnt : reArkCnt,   slimitCnt : rslArkCnt,   limitCnt : rlArkCnt,   collabCnt : rcArkCnt,   title : "R(" + rArkTcnt +")"     },
 };
 
-let unitHtml = "";
-let arkHtml = "";
-let entireHtml = "";
+const imgUnitPath = '" src="./img/unit/';
+const imgArkPath  = '" src="./img/ark/';
+let unitHtml = '';
+let arkHtml = '';
+let entireHtml = '';
+let returnHtml = '';   
+let isIactive = '';
+let index = 0;
 
 function fncRenderHtml(){
-    const div_unit = document.querySelector("#unit");
-    const div_ark = document.querySelector("#ark");
+    const div_unit   = document.querySelector("#unit");
+    const div_ark    = document.querySelector("#ark");
     const div_entire = document.querySelector("#scDiv");
-    let index = 0;
 
     document.querySelector("#maxUnit").innerHTML = maxUnitCnt;
     document.querySelector("#maxArk").innerHTML = maxArkCnt;            
@@ -81,13 +84,14 @@ function fncRenderHtml(){
     div_unit.insertAdjacentHTML('beforeend', unitHtml);
     div_ark.insertAdjacentHTML('beforeend', arkHtml);   
     div_entire.insertAdjacentHTML('beforeend', entireHtml);   
-    document.querySelector("#currunit").innerHTML = document.querySelectorAll('#unit .iactive').length;
-    document.querySelector("#currark").innerHTML = document.querySelectorAll('#ark .iactive').length;
+
+    document.querySelector("#currunit").innerHTML   = document.querySelectorAll('#unit .iactive').length;
+    document.querySelector("#currark").innerHTML    = document.querySelectorAll('#ark .iactive').length;
     document.querySelector("#currentire").innerHTML = document.querySelectorAll('#entire .iactive').length;
 }
 
 function fncCreateTitleHtml(index, title){
-    let returnHtml = "";   
+    returnHtml = '';   
     returnHtml += '<div class="container-fluid">'+
                         '<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">'+
                             '<div class="panel panel-default">'+
@@ -100,16 +104,17 @@ function fncCreateTitleHtml(index, title){
 }
 
 function fncCreateUnitHtml(item, index){                        
-    let returnHtml = "";  
-    let isIactive = "";
+     returnHtml = '';  
     if(item.cnt > 0){                
         returnHtml += fncCreateTitleHtml(index, item.title);                            
         for(let i = 1; i <= item.cnt; ++i){
-            isIactive = localStorage.getItem("unit" + (item.rate + i)) !== null;
-            returnHtml += '<img class="unit '+ (isIactive ? "iactive" : "") + '" id="unit'+(item.rate + i)+'" src="./unit/'+item.rate+'/'+i+'.png" onclick="fncSelect(unit'+(item.rate + i)+',0)">'; 
+            isIactive = localStorage.getItem("unit_" + (item.rate + i)) !== null;
+            returnHtml += '<img class="unit '+ (isIactive ? 'iactive' : '') 
+                            +'" id="unit_'+(item.rate + i) + imgUnitPath + item.rate+'/'+i+'.png" onclick="fncSelect(unit_'+(item.rate + i)+',0)">'; 
 
-            isIactive = localStorage.getItem("entire" + (item.rate + i)) !== null;
-            entireHtml += '<img class="unit '+ (isIactive ? "iactive" : "") + '" id="entire'+(item.rate + i)+'" src="./unit/'+item.rate+'/'+i+'.png" onclick="fncSelect(entire'+(item.rate + i)+',2)">'; 
+            isIactive = localStorage.getItem("entire_" + (item.rate + i)) !== null;
+            entireHtml += '<img class="unit '+ (isIactive ? 'iactive' : '')
+                            +'" id="entire_'+(item.rate + i) + imgUnitPath + item.rate+'/'+i+'.png" onclick="fncSelect(entire_'+(item.rate + i)+',2)">'; 
         }
         returnHtml += '</div></div></div></div></div>';
     }         
@@ -117,56 +122,67 @@ function fncCreateUnitHtml(item, index){
 }
 
 function fncCreateArkHtml(item, index){            
-    let returnHtml = "";               
-    if(item.tcnt > 0){
+    returnHtml = '';               
+    if(item.tcnt > 0)
+    {
         returnHtml += fncCreateTitleHtml("A" + index, item.title); 
         returnHtml += item.gcnt > 0 ? '<p class="pst">통상</p>' : '';
         for(let i = 1; i <= item.gcnt; ++i){ 
-            isIactive = localStorage.getItem("ark" + (item.rate + i)) !== null;
-            returnHtml += '<img class="'+ (isIactive ? "iactive" : "") + '" id="ark'+(item.rate+ i)+'" src="./ark/'+ item.rate +'/'+i+'.png" onclick="fncSelect('+("ark"+item.rate+i)+',1)">'; 
+            isIactive = localStorage.getItem("ark_" + (item.rate + i)) !== null;
+            returnHtml += '<img class="'+ (isIactive ? 'iactive' : '') 
+                            +'" id="ark_'+(item.rate+ i) + imgArkPath + item.rate +'/'+i+'.png" onclick="fncSelect('+("ark_"+item.rate+i)+',1)">'; 
 
-            isIactive = localStorage.getItem("entire" + (item.rate + i)) !== null;
-            entireHtml += '<img class="'+ (isIactive ? "iactive" : "") + '" id="entire'+(item.rate+ i)+'" src="./ark/'+ item.rate +'/'+i+'.png" onclick="fncSelect('+("entire"+item.rate+i)+',2)">';
+            isIactive = localStorage.getItem("entire_" + (item.rate + i)) !== null;
+            entireHtml += '<img class="'+ (isIactive ? 'iactive' : '') 
+                            +'" id="entire_'+(item.rate+ i) + imgArkPath + item.rate +'/'+i+'.png" onclick="fncSelect('+("entire_"+item.rate+i)+',2)">';
         }
         if(item.eCnt > 0){ 
             returnHtml += item.gcnt > 0 ? '<br>' : '';
             returnHtml += '<p class="pst">이벤트</p>';
             for(let i = 1; i <= item.eCnt; ++i){ 
-                isIactive = localStorage.getItem("arke" + (item.rate + i)) !== null;
-                returnHtml += '<img class="'+ (isIactive ? "iactive" : "") + '" id="arke'+(item.rate+ i)+'" src="./ark/'+ item.rate +'/e/'+i+'.png" onclick="fncSelect('+("arke"+item.rate+i)+',1)">';  
+                isIactive = localStorage.getItem("ark_e" + (item.rate + i)) !== null;
+                returnHtml += '<img class="'+ (isIactive ? 'iactive' : '') 
+                                +'" id="ark_e'+(item.rate+ i) + imgArkPath + item.rate +'/e/'+i+'.png" onclick="fncSelect('+("ark_e"+item.rate+i)+',1)">';  
 
-                isIactive = localStorage.getItem("entiree" + (item.rate + i)) !== null;                       
-                entireHtml += '<img class="'+ (isIactive ? "iactive" : "") + '" id="entiree'+(item.rate+ i)+'" src="./ark/'+ item.rate +'/e/'+i+'.png" onclick="fncSelect('+("entiree"+item.rate+i)+',2)">';                                                 
+                isIactive = localStorage.getItem("entire_e" + (item.rate + i)) !== null;                       
+                entireHtml += '<img class="'+ (isIactive ? 'iactive' : '') 
+                                +'" id="entire_e'+(item.rate+ i) + imgArkPath + item.rate +'/e/'+i+'.png" onclick="fncSelect('+("entire_e"+item.rate+i)+',2)">';                                                 
             }
         }
         if(item.limitCnt > 0){ 
             returnHtml += '<br><p class="pst">한정</p>';
             for(let i = 1; i <= item.slimitCnt; ++i){ 
-                isIactive = localStorage.getItem("arkl" + (item.rate + i)) !== null;
-                returnHtml += '<img class="'+ (isIactive ? "iactive" : "") + '" id="arkl'+(item.rate+ i)+'" src="./ark/'+ item.rate +'/l/'+i+'.png" onclick="fncSelect('+("arkl"+item.rate+i)+',1)">';  
+                isIactive = localStorage.getItem("ark_l" + (item.rate + i)) !== null;
+                returnHtml += '<img class="'+ (isIactive ? 'iactive' : '') 
+                                +'" id="ark_l'+(item.rate+ i) + imgArkPath + item.rate +'/l/'+i+'.png" onclick="fncSelect('+("ark_l"+item.rate+i)+',1)">';  
 
-                isIactive = localStorage.getItem("entirel" + (item.rate + i)) !== null;                       
-                entireHtml += '<img class="'+ (isIactive ? "iactive" : "") + '" id="entirel'+(item.rate+ i)+'" src="./ark/'+ item.rate +'/l/'+i+'.png" onclick="fncSelect('+("entirel"+item.rate+i)+',2)">';                                                 
+                isIactive = localStorage.getItem("entire_l" + (item.rate + i)) !== null;                       
+                entireHtml += '<img class="'+ (isIactive ? 'iactive' : '') 
+                                +'" id="entire_l'+(item.rate+ i) + imgArkPath + item.rate +'/l/'+i+'.png" onclick="fncSelect('+("entire_l"+item.rate+i)+',2)">';                                                 
             }
         }
         if(item.slimitCnt > 0){ 
             returnHtml += '<br><p class="pst">계절 한정</p>';
             for(let i = 1; i <= item.slimitCnt; ++i){ 
-                isIactive = localStorage.getItem("arksl" + (item.rate + i)) !== null;
-                returnHtml += '<img class="'+ (isIactive ? "iactive" : "") + '" id="arksl'+(item.rate+ i)+'" src="./ark/'+ item.rate +'/sl/'+i+'.png" onclick="fncSelect('+("arksl"+item.rate+i)+',1)">';  
+                isIactive = localStorage.getItem("ark_sl" + (item.rate + i)) !== null;
+                returnHtml += '<img class="'+ (isIactive ? 'iactive' : '') 
+                                +'" id="arks_sl'+(item.rate+ i) + imgArkPath + item.rate +'/sl/'+i+'.png" onclick="fncSelect('+("arks_sl"+item.rate+i)+',1)">';  
 
-                isIactive = localStorage.getItem("entiresl" + (item.rate + i)) !== null;                       
-                entireHtml += '<img class="'+ (isIactive ? "iactive" : "") + '" id="entiresl'+(item.rate+ i)+'" src="./ark/'+ item.rate +'/sl/'+i+'.png" onclick="fncSelect('+("entiresl"+item.rate+i)+',2)">';                                                 
+                isIactive = localStorage.getItem("entire_sl" + (item.rate + i)) !== null;                       
+                entireHtml += '<img class="'+ (isIactive ? 'iactive' : '') 
+                                +'" id="entire_sl'+(item.rate+ i) + imgArkPath + item.rate +'/sl/'+i+'.png" onclick="fncSelect('+("entire_sl"+item.rate+i)+',2)">';                                                 
             }
         }
         if(item.collabCnt > 0){ 
             returnHtml += '<br><p class="pst">콜라보</p>';
             for(let i = 1; i <= item.collabCnt; ++i){ 
-                isIactive = localStorage.getItem("arkc" + (item.rate + i)) !== null;
-                returnHtml += '<img class="'+ (isIactive ? "iactive" : "") + '" id="arkc'+(item.rate+i)+'" src="./ark/'+ item.rate +'/c/'+i+'.png" onclick="fncSelect('+("arkc"+item.rate+i) + ',1)">'; 
+                isIactive = localStorage.getItem("ark_c" + (item.rate + i)) !== null;
+                returnHtml += '<img class="'+ (isIactive ? 'iactive' : '') 
+                                +'" id="ark_c'+(item.rate+i) + imgArkPath + item.rate +'/c/'+i+'.png" onclick="fncSelect('+("ark_c"+item.rate+i) + ',1)">'; 
 
-                isIactive = localStorage.getItem("entirec" + (item.rate + i)) !== null;                       
-                entireHtml += '<img class="'+ (isIactive ? "iactive" : "") + '" id="entirec'+(item.rate+i)+'" src="./ark/'+ item.rate +'/c/'+i+'.png" onclick="fncSelect('+("entirec"+item.rate+i) + ',2)">'; 
+                isIactive = localStorage.getItem("entire_c" + (item.rate + i)) !== null;                       
+                entireHtml += '<img class="'+ (isIactive ? 'iactive' : '') 
+                                +'" id="entire_c'+(item.rate+i) + imgArkPath + item.rate +'/c/'+i+'.png" onclick="fncSelect('+("entire_c"+item.rate+i) + ',2)">'; 
             }
         }
         returnHtml += '</div></div></div></div></div>';
@@ -175,39 +191,98 @@ function fncCreateArkHtml(item, index){
 }
 
 function fncSelect(target, isType){        
-    let branch = "#curr" + (isType == 0 ? "unit" : isType == 1 ? "ark" : "entire");
-    let currCnt = Number(document.querySelector(branch).innerHTML);
-    if(localStorage.getItem(target.id) !== null){
-        target.style.opacity = 0.3;
-        document.querySelector(branch).innerHTML = currCnt-1;
+    let branch = '#curr' + (isType == 0 ? 'unit' : 'ark');
+
+    if(localStorage.getItem(target.id) !== null){                
+        target.style.opacity = 0.3;        
         localStorage.removeItem(target.id);
     }   
-    else{
-        target.style.opacity = 1;
-        document.querySelector(branch).innerHTML = currCnt+1;
-        localStorage.setItem(target.id, "");
-    }       
+    else{   
+        target.style.opacity = 1;        
+        localStorage.setItem(target.id, '');
+    }           
+    fncCheckOtherItems(target);
+    fncReloadCnt();    
+    return false;
+}
+
+function fncCheckOtherItems(target){       
+    let splitCheckId = target.id.split('_')[1]; 
+    document.querySelectorAll('div img').forEach(function(e)
+    {
+        if(target.id !== e.id && splitCheckId === e.id.split('_')[1])
+        {
+             let isCheck = localStorage.getItem(e.id) == null;
+             if(isCheck){
+                 e.style.opacity = 1;
+                 localStorage.setItem(e.id, '');
+             }
+             else{
+                 e.style.opacity = 0.3;
+                 localStorage.removeItem(e.id);
+             }
+             
+        }
+     });
     return false;
 }
 
 function fncAllSelect(type){
-    let saveType = type == "entire" ?  "scDiv" : type;            
-    document.querySelector("#curr" + type).innerHTML = (saveType === "unit" ? maxUnitCnt :  saveType === "ark" ? maxArkCnt : maxUnitCnt + maxArkCnt);
-    document.querySelectorAll("#" +  type + " img").forEach(function(item){ item.style.opacity = 1 }); 
-    document.querySelectorAll("#" + saveType + " img").forEach(function(e){ localStorage.setItem(e.id, ""); })
+    if(type == 'entire'){
+        document.querySelectorAll('div img').forEach(function(e){           
+            localStorage.setItem(e.id, ''); 
+            e.style.opacity = 1;
+        });
+    }
+    else{
+        document.querySelectorAll('#' + type + ' img').forEach(function(e){         
+            localStorage.setItem(e.id, '');                 
+            localStorage.setItem('entire_' + e.id.split('_')[1], ''); 
+            e.style.opacity = 1;
+            document.querySelector('#entire_' + e.id.split('_')[1]).style.opacity = 1;            
+        });
+    }    
+    
+    fncReloadCnt();
     return false;
 }
 function fncReset(type){
-    let saveType = type == "entire" ?  "scDiv" : type;     
-    document.querySelector("#curr" + type).innerHTML = 0;
-    document.querySelectorAll("#" +  type + " img").forEach(function(item){ item.style.opacity = 0.3 }); 
-    document.querySelectorAll("#" + saveType + " img").forEach(function(e){ localStorage.removeItem(e.id); })
+    if(type == 'entire'){
+        localStorage.clear();    
+        document.querySelectorAll('span[id^="curr"]').forEach(function(e){ e.innerHTML = 0; });
+        document.querySelectorAll('img').forEach(function(e){ e.style.opacity = 0.3; })
+    }
+    else{
+        document.querySelector('#curr' + type).innerHTML = '0';
+        document.querySelectorAll('#' + type + ' img').forEach(function(e){             
+            localStorage.removeItem(e.id); 
+            localStorage.removeItem('entire' + e.id.split('_')[1]); 
+            document.querySelector('#entire_' + e.id.split('_')[1]).style.opacity = 0.3;
+            e.style.opacity = 0.3
+        })
+    }
+    return false;
+}
+
+function fncReloadCnt(){    
+    let entireCnt = 0;
+    let arkCnt = 0;
+    let unitCnt = 0;
+
+    document.querySelectorAll('#scDiv img').forEach(function(e){ if(e.style.opacity == 1) entireCnt++; });
+    document.querySelectorAll('#ark   img').forEach(function(e){ if(e.style.opacity == 1) arkCnt++;  });
+    document.querySelectorAll('#unit  img').forEach(function(e){ if(e.style.opacity == 1) unitCnt++; });
+
+    document.querySelector('#currentire').innerHTML = entireCnt;
+    document.querySelector('#currark').innerHTML    = arkCnt;
+    document.querySelector('#currunit').innerHTML   = unitCnt;
+
     return false;
 }
 
 function fncDownload(div){
-    domtoimage.toBlob(document.getElementById(div)).then(function (blob) {
-        window.saveAs(blob, 'LastCloudia_KR_CheckList.png');
-    });
+    domtoimage.toBlob(document.getElementById(div)).then(function (blob) { window.saveAs(blob, 'LastCloudia_KOR_CheckList.png'); });
     return false;
 }
+
+
